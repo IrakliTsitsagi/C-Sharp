7f7f722e9c2a671122b1cf60a059947dc4c45826
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-03-30 16:23:17 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6;
+            string name = "Ahmed";
+
+            var fullName = "Dejan Stankovic";
+
+            Console.WriteLine(fullName);
+
+            var test = 10.5;  // rasac ginda dawer ricxvi dabli inti stringi. var ro uweria win sio
+            Console.WriteLine(test);
+           
+        }
+    }
+}
