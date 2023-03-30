40449d1c8e737fcd7cfe8a93bbe9af5cdf86b4e7
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-03-30 16:24:06 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int serialNo = 100;
+            string accountNo = "";
+            float anyNo = 3.5f;
+            accountNo = serialNo.ToString();
+
+            Console.WriteLine(accountNo);
+
+
+            // serialNo = Convert.ToInt32(anyNo); es amrgvalebs ricxvs anu gamoitans 3.5 ze 4 s
+
+            // es qvemot ar amrgvalebs. 3.5 gamoitans 3
+            serialNo = (int)anyNo;
+
+            Console.WriteLine(serialNo);
+        }
+    }
+}
