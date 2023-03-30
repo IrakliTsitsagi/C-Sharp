b76fe5956b59dad313b22db18cbc06a5f7a9bd27
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-03-30 16:19:22 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("Number x is " + x);
+            x = 5;
+            Console.WriteLine("Number x is " + x);
+
+            double double_num = 0.0d;
+            float float_num = 0.0f;
+            decimal decimal_num = 0.0m;
+
+            double_num = 9.8;
+            float_num = 5.5f;
+            decimal_num = 100.250m;
+
+            Console.WriteLine("double is " + double_num + " float is " + float_num + " decimal is " + decimal_num);
+
+            string firstName, lastName, house;
+            firstName = "jon";
+            lastName = "snow";
+            house = "stark";
+
+            Console.WriteLine("Hello " + firstName + " " + lastName + " of house " + house);
+
+            bool isWorking;
+            isWorking = true;
+
+            Console.WriteLine(isWorking);
+
+            int sum;
+            sum = 0;
+
+            int number1, number2;
+            number1 = 5;
+            number2 = 8;
+            sum = number1 + number2;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
