4100141382806d018b9384f71d4d63e34725d8d2
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-03-30 16:27:26 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DateTimeLecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime myDate = new DateTime(2023,5,1); // weli tve ricxvi
+            DateTime myDate = DateTime.Now; //DateTime.Today; <-es saats ar achvenebs 
+            string formattedDate = string.Format("date is {0:yyyy MMMM dd dddd mm ss}", myDate); /* sxvadasxva 
+            formatebi */
+            Console.WriteLine(formattedDate); 
+        }
+    }
+}
