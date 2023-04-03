0091682626a77d225b186a9a46625abcaadd69f7
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-03 17:12:40 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome {0}", name);
+        }
+    }
+}
