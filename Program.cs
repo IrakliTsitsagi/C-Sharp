3b4650bb4fc2c3ca02ef0dd6b0ad7f5aca644ccd
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-03 20:31:24 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MoreLogicalOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool username = false;
+            bool email = false;
+            bool password = true;
+
+            if ((username || email) && password)
+                Console.WriteLine("user is valid");
+
+            else
+                Console.WriteLine("something went wrong");
+
+            
+        }
+    }
+}
