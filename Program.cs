b44593f4d61dce1b9b2895337f4c777e035353a0
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-03 17:17:36 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ComparisonOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+
+            x = 9; y = 7;
+
+            if (x == y)
+                Console.WriteLine("X is equal to Y");
+            else if (x != y)
+            {
+                // nested if
+                if (x > y)
+                    Console.WriteLine("X is greater than Y");
+                else if (x < y)
+                    Console.WriteLine("X is less than Y");
+
+            }
+        }
+    }
+}
