b6d91c3fd30fd0987e8192f8bf1f36aee47c9c1a
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-03 17:10:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are You Gay? (Yes or No)");
+            String input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Yes": Console.WriteLine("Why Are You Gay?"); break;
+                case "No": Console.WriteLine("Then Who Is Gay?"); break;
+                default: Console.WriteLine("rai bijoo?"); break;
+
+            }
+
+        }
+    }
+}
