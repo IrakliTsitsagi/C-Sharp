bd48749c332b7240f65709d05540071d6aa77019
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-03 21:30:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AssignmentNo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you {0} My name is C#, How old are you?", name);
+            int.TryParse(Console.ReadLine(), out int age);
+            if (age == 0)
+                Console.WriteLine("That is not a correct input");
+            else
+                Console.WriteLine("{0} good, As for me I was born on 2002. Which makes me {1} years old.", age, DateTime.Now.Year - 2002);
+        }
+    }
+}
