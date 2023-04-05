747c5adaf9e3186f57f623e3cb6845335f28a595
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-05 15:39:28 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace While_DoWhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            int x = 1;
+            while (x <= 10)
+            {
+                if (x == 7)
+                    break;
+                Console.WriteLine("this is line no. " + x);
+                x++;
+            }
+            Console.WriteLine("_________");
+
+
+            //do while loop
+
+            int y = 1;
+
+            do
+            {
+                Console.WriteLine("es aris xazi nomeri " + y);
+                y++;
+            } while (y <= 9);
+        }
+
+    }
+}
