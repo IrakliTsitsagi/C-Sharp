425fff964197d34b02d480a73f2a18658b241528
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-05 14:29:00 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringManipulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvShow = "game of thrones     ";
+            string seasonOne = " - Season 1";
+            string newName = "";
+
+            newName = tvShow.Insert(tvShow.Trim().Length, seasonOne);
+            Console.WriteLine(newName);
+
+        }
+    }
+}
