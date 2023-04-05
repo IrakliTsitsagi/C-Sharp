486f1fd03e1fd0061e191ea332165fa035272c3d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-05 11:58:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int.TryParse(Console.ReadLine(), out int x);
+            if (x == 0)
+            {
+                Console.WriteLine("not valid input");
+            }
+            else
+            Console.WriteLine("You have entered no. " + x);
+        }
+    }
+}
