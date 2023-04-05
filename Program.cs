33f1b24ebae6968294b3c30525e604d17977c65e
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-05 12:54:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringManipulationPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvShow = "game of thrones";
+
+            // index of
+            Console.WriteLine(tvShow.IndexOf('o'));
+
+            //last index of
+            Console.WriteLine(tvShow.LastIndexOf('o'));
+
+            // substring anu piuvlel asoebs ar dawers tuminda
+            Console.WriteLine(tvShow.Substring(4));
+            Console.WriteLine(tvShow.Substring(4, 9));
+
+            // remove
+            Console.WriteLine(tvShow.Substring(7));
+
+            //insert sityvas amateb
+            Console.WriteLine(tvShow.Insert(0,"the "));
+            Console.WriteLine(tvShow.Insert(8, "the "));
+        }
+    }
+}
