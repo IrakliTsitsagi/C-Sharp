946289153317b4146a5a4e524b3d7a4717a03ee7
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-06 15:58:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                // Console.WriteLine("First loop i = " + i);
+                for (int j = 0; j <= 7; j++)
+                {
+                    Console.Write("0 ");
+                    // Console.WriteLine("Second loop j = " + j);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
